010bedaf98f1713558b85ef4a5304ba0e2f702a1
Michael Gearhardt <michael.gearhardt@fathym.com>
2019-02-06 12:02:27 -0700

    Missing imports

--- projects/json-schema-editor/src/lib/json-schema-editor.module.ts
+++ projects/json-schema-editor/src/lib/json-schema-editor.module.ts
@@ -3,12 +3,12 @@ import { NgModule } from '@angular/core';
 import { JSONSchemaFormControlsComponent } from './json-schema-form-controls/json-schema-form-controls.component';
 import { DndModule } from '@beyerleinf/ngx-dnd';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { MatCardModule, MatSelectModule, MatIconModule, MatSlideToggleModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+import { MatCardModule, MatSelectModule, MatIconModule, MatSlideToggleModule, MatButtonModule, MatFormFieldModule, MatInputModule, MatCheckboxModule } from '@angular/material';
 import { FathymSharedModule } from '@lcu/hosting';
 
 @NgModule({
   declarations: [JSONSchemaEditorComponent, JSONSchemaFormControlsComponent],
-  imports: [FathymSharedModule, DndModule, FlexLayoutModule, MatButtonModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatSlideToggleModule],
+  imports: [FathymSharedModule, DndModule, FlexLayoutModule, MatButtonModule, MatCardModule, MatCheckboxModule, MatFormFieldModule, MatIconModule, MatInputModule, MatSelectModule, MatSlideToggleModule],
   exports: [JSONSchemaEditorComponent, JSONSchemaFormControlsComponent]
 })
 export class JsonSchemaEditorModule {}
