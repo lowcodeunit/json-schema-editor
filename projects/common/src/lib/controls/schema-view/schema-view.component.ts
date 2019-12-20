b155b4aea67ebc9cc76d2c1a16cfbc58b3e2fe9f
xchanin <me@shannonbruns.com>
2019-12-20 15:25:06 -0700

    added id and name properties for automation testing

--- projects/common/src/lib/controls/schema-view/schema-view.component.ts
+++ projects/common/src/lib/controls/schema-view/schema-view.component.ts
@@ -8,25 +8,68 @@ import { JSONSchema } from '@lcu/common';
 })
 export class SchemaViewComponent implements OnInit {
 
-  private _schema: JSONSchema;
-
-  @Input('stringify-schema')
-  public StringifySchema: boolean;
-
+  private _schema: JSONSchema | string;
   @Input('schema')
-  public set Schema(val: JSONSchema) {
-    this._schema = val;
-    // this._schema = JSON.stringify(val, null, 5);
+  public set Schema(val: JSONSchema | string) {
+      this._schema = val;
   }
 
-  public get Schema(): JSONSchema {
+  public get Schema(): JSONSchema | string {
     return this._schema;
   }
 
-  constructor() { }
-
-  ngOnInit() {
-    console.log(this.Schema);
+  private _editSchema: boolean;
+  @Input('edit-schema')
+  public set EditSchema(val: boolean) {
+    this._editSchema = val;
   }
 
+  public get EditSchema(): boolean {
+    return this._editSchema;
+  }
+
+  /**
+   * Schema as a string
+   */
+  // private _schemaString: string;
+  // public set SchemaString(val: string) {
+  //   this._schemaString = val;
+  //   this.schemaStringChange(val);
+  // }
+
+  // public get SchemaString(): string {
+  //   return this._schemaString;
+  // }
+
+  /**
+   * Edit button Tooltip message
+   */
+  public EditTooltip: string;
+
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  /**
+   * When changing schema string
+   */
+  public schemaStringChange(val: string): void {
+    // this.Schema = val as JSONSchema;
+    // console.log('schema', this.Schema);
+  }
+
+  /**
+   * Edit schema
+   */
+  public Edit(): void {
+  //   this.EditSchema = !this.EditSchema;
+
+  //   if (this.EditSchema) {
+  //     this.EditTooltip = 'Cancel Edit';
+  //     this.SchemaString = JSON.stringify(this.Schema, null, 5);
+  //   } else {
+  //     this.EditTooltip = 'Edit Schema';
+  //   }
+  }
 }
